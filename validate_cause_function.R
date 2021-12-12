91680c4a8dbb06df031a3b08d4e69a9d28c91707
Rose Bender <rbender1@uw.edu>
2021-12-12 15:20:18 -0800

    edit function output to include error_rows and error_text

--- validate_cause_function.R
+++ validate_cause_function.R
@@ -5,7 +5,7 @@
 #' @ Outputs: TRUE/FALSE
 ########################################################################################################################################################
 ## Load packages
-pacman::p_load(data.table, openxlsx, validate)
+pacman::p_load(data.table, openxlsx)
 
 ## Set up inputs
 dt <- as.data.table(read.xlsx(paste0("/ihme/homes/rbender1/leprosy_extracted_Wkly-Epi-Rcrd_GBD2019.xlsx")))
@@ -16,12 +16,19 @@ validation_criteria <- list('age_start >= 0',
                             'severity %in% c("G2DN", "G<2D") | is.na(severity)')
 
 validation_check <- function(dt, validation_criteria) {
+  error_rows <- list()
+  error_text <- list()
   for(v in validation_criteria){
     dt[, meet_criteria := 0]
     dt[eval(parse(text = v)), meet_criteria := 1]
     if(any(dt$meet_criteria == 0)){
-      print(paste("You have", nrow(dt[meet_criteria == 0]), "observations that do not meet", v, "in these rows:", paste(which(dt$meet_criteria == 0), collapse = ", ")))
+      error_text[[v]] <- paste("You have", nrow(dt[meet_criteria == 0]), "observations that do not meet", v, "in these rows:", paste(which(dt$meet_criteria == 0), collapse = ", "))
+      print(error_text[[v]])
     } 
+    error_rows[[v]] <- copy(dt[meet_criteria == 0])
   }
-  return(dt)
+  dt$meet_criteria <- NULL
+  return(list(data = dt,
+              error_rows = error_rows, 
+              error_text = error_text))
 }
