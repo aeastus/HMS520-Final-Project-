1ab53ec397acbf9777785fd733cf8e2eb83fadab
aeastus <90588979+aeastus@users.noreply.github.com>
2021-12-13 18:33:11 -0800

    fixing my mistake

--- save_report.R
+++ save_report.R
@@ -5,33 +5,6 @@
 #' @ Notes: : Run check functions for errors/missing data and other problems in the data set. 
 ########################################################################################################################################################
 ## Load packages
-<<<<<<< HEAD
-pacman::p_load(data.table, openxlsx, knitr)
-
-#Validation final check
-test_dt <- validation_check(dt, validation_criteria)
-all_rows_with_errors <-  rbindlist(test_dt[["error_rows"]])
-all_rows_with_errors <- all_rows_with_errors[!duplicated(all_rows_with_errors)]
-test_dt[["error_text"]]
-
-#duplicate final check 
-test2_dt <- duplicate_check(dt, byvars)
-all_rows_with_errors <-  rbindlist(test2_dt[["error_rows"]])
-all_rows_with_errors <- all_rows_with_errors[!duplicated(all_rows_with_errors)]
-test2_dt[["error_text"]]
-
-#missing final check 
-test3_dt <- missing_check(dt, vars_check)
-all_rows_with_errors <-  rbindlist(test3_dt[["error_rows"]])
-all_rows_with_errors <- all_rows_with_errors[!duplicated(all_rows_with_errors)]
-test3_dt[["error_text"]]
-
-#outlier final check 
-test4_dt <- outlier_checkk(dt, byvars, release_id, n = 3, flag_zeros = TRUE)
-all_rows_with_errors <-  rbindlist(test4_dt[["error_rows"]])
-all_rows_with_errors <- all_rows_with_errors[!duplicated(all_rows_with_errors)]
-test4_dt[["error_text"]]
-=======
 pacman::p_load(data.table, openxlsx, knitr, rmarkdown)
 
 ## Source functions
@@ -76,13 +49,12 @@ print_outputs <- function(list){
 
 lapply(list_of_outputs, print_outputs)
 
->>>>>>> origin/main
 
 #Write a report with the final check into PDF (also Rmarkdown option below)
 rmarkdown::render(input = paste0(source_dir, "save_report.R"), output_format = "pdf_document", output_dir = input_dir)
 
 #Report --> Rmarkdown
-rmarkdown::render("save_report.R", "html")
+rmarkdown::render("save_report.R", "rmarkdown")
 
 
 
